e17b340d4456ef0deffeb81c75b1f887c3aa8ade
Mudassar Amin <mudasir45@users.noreply.github.com>
2023-08-28 13:23:47 +0500

    commit

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 def postlist(request):
-    return render(request, 'post_list.html')
+    posts = Post.objects.all()
+    context = {
+        'posts' : posts,
+    }
+    return render(request, 'post_list.html', context)
 
 def post(request):
     return render(request, 'post.html')
