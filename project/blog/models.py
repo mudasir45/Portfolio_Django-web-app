4e845f65ce03a91fcad7499b47bd9322381f45eb
Mudassar Amin <mudasir45@users.noreply.github.com>
2023-08-28 11:37:10 +0500

    commit

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Tags(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Post(models.Model):
+    headline = models.CharField(max_length=200)
+    sub_headline = models.CharField(max_length=200, null=True, blank=True)
+    # thumbnail = 
+    body = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tags, blank=True)
+    # slug = 
+
+    def __str__(self) -> str:
+        return self.headline
+    
