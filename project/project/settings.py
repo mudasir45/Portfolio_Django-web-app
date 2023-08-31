5097a64b3c28011d69f2bcb738f4ba04b71e7f00
Mudassar Amin <mudasir45@users.noreply.github.com>
2023-08-31 14:26:00 +0500

    commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -92,23 +92,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_portfolio_db',
-        'USER': 'postgres',
-        'PASSWORD': '79615Postgres',
-        'HOST': 'dpdatabase.cmci5jfqvkak.eu-north-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'django_portfolio_db',
+#         'USER': 'postgres',
+#         'PASSWORD': '79615Postgres',
+#         'HOST': 'dpdatabase.cmci5jfqvkak.eu-north-1.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
